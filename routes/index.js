be9611761fd083d9eb868e58f77b18c8dce37bf8
Lilynews <lilinews2021@gmail.com>
2023-02-14 11:43:21 -0500

    feat: project init

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const home = require('./modules/home')
+// const detail = require('./modules/detail')
+// const users = require('./modules/users')
+
+router.use('/', home)
+// router.use('/detail', detail)
+// router.use('/users', users)
+
+module.exports = router
