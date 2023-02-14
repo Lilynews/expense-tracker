4e2ec0b8c3a1677c9f3ca55d47e93c623f957931
Lilynews <lilinews2021@gmail.com>
2023-02-14 15:36:59 -0500

    feat: import handlebars

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const express = require('express')
 const app = express()
 
 const port = 3000
+
+//////// router setting
 const routes = require('./routes')
 
+//////// handlebars setting
+const exphbs = require('express-handlebars');
+
+
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+//////// after import, use in this project
+app.use(express.static('public'))
 app.use(routes)
 
 //////// setting listening
