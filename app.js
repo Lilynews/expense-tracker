c8c99d4b9d24b30de4e7f0dba28f0c001d52bc13
Lilynews <lilinews2021@gmail.com>
2023-02-16 11:33:26 -0500

    feat: import passport & session & app.js setting

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
-//////// import express
+// import express
 const express = require('express')
 const app = express()
-
 const port = 3000
-
-//////// router setting
-const routes = require('./routes')
-
-//////// handlebars setting
+// import handlebars 
 const exphbs = require('express-handlebars');
+// router setting
+const routes = require('./routes')
+// import session & passport for login feature
+const session = require('express-session')
+const usePassport = require('./config/passport')
 
+// import flash
+const flash = require('connect-flash')
 
+// for environment variable
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
+// executed mongoose
+require('./config/mongoose')
+
+// setting express-handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-//////// after import, use in this project
+// for static files
 app.use(express.static('public'))
+// for body.parser
+app.use(express.urlencoded({ extended: true }))
+
+// session setting
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// passport setting, after session
+usePassport(app)
+
+// flash setting
+app.use(flash())  
+
+// middleware setting
+app.use((req, res, next) => {
+  // for auth
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  // for flash
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
+// import all request into routes
 app.use(routes)
 
-//////// setting listening
+// setting listening
 app.listen(port, () => {
-  console.log('App is running on http://localhost:3000')
+  console.log(`Express is listening on localhost:${port}`)
 })
---
+++ config/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
+
+module.exports = app => {
+  // Passport init
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // setting local strategy
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          return done(null, false, req.flash('error', 'Email or password error！'))
+        }
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, req.flash('error', 'Email or password error'))
+          }
+          return done(null, user)
+        })
+      })
+      .catch(err => done(err, false))
+  }))
+  // setting serialize or deserialize
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
+  })
+
+  // FB
+
+
+}
