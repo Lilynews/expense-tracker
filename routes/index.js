ec2453cd3f337b10898608335c7ad4c268e0a689
Lilynews <lilinews2021@gmail.com>
2023-02-16 21:56:50 -0500

    feat: add user auth path at expense router

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-// const expenses = require('./modules/expenses')
+const expenses = require('./modules/expenses')
 const users = require('./modules/users')
 // middleware
 const { authenticator } = require('../middleware/auth')  
 
-// router.use('/expenses', expenses)
+router.use('/expenses', authenticator, expenses)
 router.use('/users', users)
 router.use('/', authenticator, home)
 
