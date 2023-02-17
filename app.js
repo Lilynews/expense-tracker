e1a8c0cee338d9633d5d1de74486f4bce64eca44
Lilynews <lilinews2021@gmail.com>
2023-02-16 22:28:41 -0500

    feat: add .env.example

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(methodOverride('_method'))
 
 // session setting
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
