04d57fa0d2d48ad1cca1d2605696ca4e9af41b47
Lilynews <lilinews2021@gmail.com>
2023-02-16 21:58:04 -0500

    feat: add home page & router

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,68 @@
 const express = require('express')
 const router = express.Router()
+const moment = require('moment')
+
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'expense-tracker' })
+  const userId = req.user._id
+
+  Category.find()
+    .lean()
+    .then(catagories => {
+      Record.find({ userId })
+        .populate('categoryId')
+        .lean()
+        .sort({ date: 'desc' }) // asc
+        .then(records => {
+          let totalAmount = 0
+          
+          records.forEach(item => {
+            totalAmount += item.amount
+            item.date = moment(item.date).format('YYYY-MM-DD')
+          })
+
+          return res.render('index', {records, catagories, totalAmount
+          })
+        })
+    })
+    .catch(error => console.error(error))
+})
+
+router.get('/search', (req, res) => {
+  const userId = req.user._id
+  const categoryId = req.query.categoryId
+
+  if (!categoryId) {
+    return res.redirect('/')
+  }
+
+  Category.find()
+    .lean()
+    .then(catagories => {
+      catagories.forEach(item => {
+        if (String(item._id) === categoryId) {
+          item.selected = true
+        } else {
+          item.selected = false
+        }
+      })
+      Record.find({ userId, categoryId })
+        .populate('categoryId')
+        .lean()
+        .sort({ date: 'desc' }) // asc
+        .then(records => {
+          let totalAmount = 0
+          records.forEach(item => {
+            totalAmount += item.amount
+            item.date = moment(item.date).format('YYYY-MM-DD')
+          })
+
+          return res.render('index', { records, catagories, totalAmount})
+        })
+    })
+    .catch(error => console.error(error))
 })
 
 module.exports = router
