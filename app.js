d27ab9bf869c383e83e52b91b5562a7e48138f9f
Lilynews <lilinews2021@gmail.com>
2023-02-16 23:46:38 -0500

    feat: update Heroku setting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import express
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 // import handlebars 
 const exphbs = require('express-handlebars')
 // import method-override for PUT & DELETE
